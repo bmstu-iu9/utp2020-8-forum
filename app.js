af0c4161a412814b2aff6a838c3f0f5a6825ef29
ohstapit <ohstapit@gmail.com>
2020-07-08 17:43:46 +0300

    Static files fix, logger implemented

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const logger = require('./middleware/logger');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger); // Показывает в консоли запросы к серверу и время запроса
+app.use(express.static(path.join(__dirname, 'public'))); // Где находятся статик-файлы (скрипты, стили, картинки и т.п.
 
-app.listen(8080);
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
---
+++ middleware/logger.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+
+const logger = (req, res, next) => {
+    console.log(
+        `${req.protocol}://${req.get('host')}${
+            req.originalUrl
+        }: ${moment().format('HH:mm:ss')}`);
+    next();
+}
+module.exports = logger;
+
