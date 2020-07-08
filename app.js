4eae7f30caee3141818cf3fbd26e8dee62c6c6f9
ohstapit <ohstapit@gmail.com>
2020-07-08 20:58:12 +0300

    Using handlebars example

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const path = require('path');
+const exphbs  = require('express-handlebars');
 const logger = require('./middleware/logger');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(logger); // Показывает в консоли запросы к серверу и время запроса
-app.use(express.static(path.join(__dirname, 'public'))); // Где находятся статик-файлы (скрипты, стили, картинки и т.п.
+app.use('/static', express.static(path.join(__dirname, 'public'))); // Где находятся статик-файлы (скрипты, стили, картинки и т.п.
+
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res, next) {
+    res.render('home', {layout: 'main', posts: ['post 1', 'post 2', 'post 3']});
+});
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
