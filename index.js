b27e1cbffd9dc25a55be8e7d3be53c2b114248c2
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2020-07-11 21:47:32 +0300

    Добавлена возможность создания новых пользователей

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ console.log('It works!');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = 8080;
 
-let db = require('./SqliteDataBase/users.js')
+let sqlite = require('./UsersDataBase/users.js')
+const parser = bodyParser.urlencoded({extended: false});
 
-//app.use(express.static(__dirname + '/public'));
+const db = sqlite.init();
 
-app.get("/api/users", (req, res, next) => {
-    console.log(db.createUser("admin", "admin@d.com", 11111));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/styles', express.static(__dirname + '/styles'));
+
+app.use('/scripts', express.static(__dirname + '/scripts'))
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/public/signup.html');
+  console.log("User is at signup page");
+});
+
+app.post('/signup', parser, (req, res) => {
+  console.log(req.body);
+  let user = req.body;
+  sqlite.createUser(db, user.login, user.email, user.psw);
+});
+
+app.get("/users", (req, res, next) => {
+  let rows = db.prepare('select * from users').all();
+  res.json({
+      "data":rows
+  });
 });
 
 app.listen(8080, () => console.log('Server running at http://localhost:8080'));
