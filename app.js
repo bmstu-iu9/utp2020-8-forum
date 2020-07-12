c50faadcdad18b7762a361d072f50ea4b334df19
Azmetov Danila <ohstapit@gmail.com>
2020-07-12 21:39:24 +0300

    static files fixed

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(logger); // Показывает в консоли запросы к серверу и время запроса
-app.use('/static', express.static(path.join(__dirname, 'public'))); // Где находятся статик-файлы (скрипты, стили, картинки и т.п.)
+app.use('/static', express.static(path.join(__dirname, '/public')))
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
 
 const db = dbManager.init();
 
