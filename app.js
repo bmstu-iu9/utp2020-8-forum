ac0553eb10a80ff73aef15a550d96fc598cba118
Maxud-M <maksud.maksudov.00@mail.ru>
2020-07-14 14:46:56 +0300

    Add processing new post and reply Closes: #6

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const logger = require('./modules/logger');
 const dbManager = require('./modules/db');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
-
+const urlencoded = bodyParser.urlencoded({extended: false});
 app.use(logger); // Показывает в консоли запросы к серверу и время запроса
 app.use('/static', express.static(path.join(__dirname, '/public')))
 
@@ -18,7 +19,7 @@ const db = dbManager.init();
 app.get('/', function (req, res) {
     let categories = dbManager.getCategories(db);
     let posts = dbManager.getAllPosts(db)
-    res.render('home', {layout: 'postsListViewLayout', posts: posts, categories: categories, postsListTitle: 'Все посты'});
+    res.render('home', {layout: 'postsListViewLayout', posts: posts, categories: categories, postsListTitle: 'Все посты', ind: 0});
 });
 
 app.get('/post/:postId', function (req, res) {
@@ -33,7 +34,27 @@ app.get('/category/:categoryId', function (req, res) {
     let categories = dbManager.getCategories(db);
     let id = req.params.categoryId;
     let posts = dbManager.getPostsByCategory(db, id)
-    res.render('home', {layout: 'postsListViewLayout', posts: posts, categories: categories, postsListTitle: categories[id-1].name});
+    res.render('home', {layout: 'postsListViewLayout', posts: posts, categories: categories, postsListTitle: categories[id-1].name, ind: 0});
+})
+
+app.post('/post/:postId', urlencoded, function (req, res){
+    let categories = dbManager.getCategories(db);
+    let id = req.params.postId;
+    let post = dbManager.getPost(db, id)
+    dbManager.addNewReply(db, 1, req.body.myAnswer, id);
+    let replies = dbManager.getReplies(db, id);
+    res.render('home', {layout: 'postViewLayout', categories: categories, post: post, replies: replies});
+})
+
+app.post('/category/:categoryId', urlencoded, function (req, res){
+    let categories = dbManager.getCategories(db);
+    let id = req.params.categoryId;
+    let ind = 0
+    if(!dbManager.addNewPost(db, 1, req.body.myPost, id)){
+        ind = 1
+    }
+    let posts = dbManager.getPostsByCategory(db, id)
+    res.render('home', {layout: 'postsListViewLayout', categories: categories, posts: posts, postsListTitle: categories[id - 1].name, ind: ind})
 })
 
 
